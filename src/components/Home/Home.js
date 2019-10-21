effc403513c64e155c688defad5cff489fbca990
Max Hebert <maxhebert@Maxs-MacBook-Pro.local>
2019-10-21 15:56:13 -0700

    first

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import store from "./../../store";
 import { Link } from "react-router-dom";
 import RecipeCard from "./../RecipeCard/RecipeCard";
 import "./Home.css";
@@ -6,8 +7,9 @@ import "./Home.css";
 class Home extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState();
     this.state = {
-      recipes: []
+      recipes: reduxState.recipes
     };
   }
 
